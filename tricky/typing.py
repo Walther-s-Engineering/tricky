779b771fdf18a9602cd8d517b21fe17970137605
Alexander Walther <asushofman@gmail.com>
2023-01-24 23:38:52 +0300

    [main] @WIP typing module

--- tricky/typing.py
+++ tricky/typing.py
@@ -1,1 +1,18 @@
-raise NotImplementedError('Sorry, this module is in progress yet.')
+import sys
+
+if sys.version_info > (3, 7):
+    class Bool(str): ...  # noqa: E701
+
+    class String(str): ...  # noqa: E701
+
+    class Integer(int): ...  # noqa: E701
+
+    class Float(str): ...  # noqa: E701
+
+    class List(list): ...  # noqa: E701
+
+if sys.version_info > (3, 8):
+    print(sys.version)
+
+if sys.version_info > (3, 9):
+    print(sys.version_info)
