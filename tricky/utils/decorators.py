1d8cd0865b21e0864a8261b2e9a41de0b40ea2f9
Alexander Walther <asushofman@gmail.com>
2023-04-10 18:46:09 +0300

    [MAIN] Added decorator to hide functions signature

--- tricky/utils/decorators.py
+++ tricky/utils/decorators.py
@@ -3,10 +3,15 @@ import functools
 import inspect
 import typing as t
 
+from types import FunctionType
+
 DefaultAny = t.TypeVar('DefaultAny')
 
 
 def suppress(default_value, exceptions: t.Tuple[t.Type[Exception]] = (Exception,)):
+    """ This decorator is useful if you need to return the default
+    value of a function despite possible errors during its execution.
+    """
     def decorating(function):
         if inspect.iscoroutinefunction(function):
             async def wrapper(*args, **kwargs) -> t.Any:
@@ -22,3 +27,22 @@ def suppress(default_value, exceptions: t.Tuple[t.Type[Exception]] = (Exception,
             return default_value
         return wrapper
     return decorating
+
+
+AnyFunction = t.TypeVar('AnyFunction', bound=FunctionType)
+
+
+def hide_signature(function: AnyFunction) -> AnyFunction:
+    """ This decorator is needed to hide the signature of the function being decorated.
+    This decorator is useful when working with FastAPI.
+    Example:
+        ```
+        @hide_signature
+        def yield_db_session(db_dsn: String) -> Session:
+            ...
+
+        def get_items(session: Session = Depends(yield_db_session()): ...
+        ```
+    """
+    function.__wrapped__ = lambda: None
+    return function
